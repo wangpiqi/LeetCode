25d8adf13aa520086e02c36efe95597d4338166a
wangpiqi <737711464@qq.com>
2020-11-01 09:05:09 +0800

    LCP 06. 拿硬币

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -6,7 +6,13 @@ using namespace std;
 
 class Solution {
 public:
+	//LCP 06. ÄÃÓ²±Ò
 	int minCount(vector<int>& coins) {
-		return 0;
+		int count{0};
+		for (auto coin : coins)
+		{
+			count += (coin >> 1) + (coin & 1);
+		}
+		return count;
 	}
 };
