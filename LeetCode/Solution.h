e46b37a4276362e1dc77dc58e99418c52b48c952
wangpiqi <737711464@qq.com>
2020-11-01 10:03:50 +0800

    1502. 判断能否形成等差数列

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -8,6 +8,24 @@ using namespace std;
 
 class Solution {
 public:
+	//1502. 判断能否形成等差数列
+	bool canMakeArithmeticProgression(vector<int>& arr) {
+		if (arr.size() < 3)
+		{
+			return true;
+		}
+
+		sort(arr.begin(), arr.end());
+
+		int diff = arr[1] - arr[0];
+		for (int i = 1; i < arr.size() - 1; i++)
+		{
+			if (arr[i + 1] - arr[i] != diff)
+				return false;
+		}
+		return true;
+	}
+
 	//1464. 数组中两元素的最大乘积
 	int maxProduct(vector<int>& nums) {
 		nth_element(nums.begin(), nums.begin() + 2, nums.end(), std::greater<int>());
