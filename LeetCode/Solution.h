bf8b586203993dbd0d02f7e866e1f76caf5392e7
wangpiqi <737711464@qq.com>
2020-11-01 08:58:30 +0800

    Architecture

---
+++ LeetCode/Solution.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int minCount(vector<int>& coins) {
+		return 0;
+	}
+};
---
+++ LeetCode/test.cpp
@@ -0,0 +1,12 @@
+#include "Solution.h"
+
+int main()
+{
+	Solution s;
+
+	vector<int> temp{ 4,2,1 };
+	s.minCount(temp);
+
+	system("pause");
+	return 0;
+}
