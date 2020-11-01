01d5f08172f44e0a72eef99b6e0aff185c66039e
wangpiqi <737711464@qq.com>
2020-11-01 10:54:20 +0800

    合并两个有序链表

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -6,8 +6,42 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
+	//21. 合并两个有序链表
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		//是否l1<l2
+		auto compare = [](ListNode* l1, ListNode* l2) -> bool {
+			if (!l1) return false;
+			if (!l2) return true;
+			return l1->val < l2->val;
+		};
+
+		ListNode* head{ compare(l1, l2) ? l1 : l2 };
+
+		ListNode* last{ nullptr };
+		while (l1 || l2)
+		{
+			ListNode*& node = compare(l1, l2) ? l1 : l2;
+			if (last)
+			{
+				last->next = node;
+			}
+			last = node;
+			node = node->next;
+		}
+		
+		return head;
+	}
+
 	//1502. 判断能否形成等差数列
 	bool canMakeArithmeticProgression(vector<int>& arr) {
 		if (arr.size() < 3)
@@ -18,7 +52,7 @@ public:
 		sort(arr.begin(), arr.end());
 
 		int diff = arr[1] - arr[0];
-		for (int i = 1; i < arr.size() - 1; i++)
+		for (int i = 1; i < (int)arr.size() - 1; i++)
 		{
 			if (arr[i + 1] - arr[i] != diff)
 				return false;
--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -5,7 +5,8 @@ int main()
 	Solution s;
 
 	vector<int> temp{ 4,2,1 };
-	s.minCount(temp);
+	//s.minCount(temp);
+	s.mergeTwoLists(nullptr, nullptr);
 
 	system("pause");
 	return 0;
