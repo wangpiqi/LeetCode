e6aead966c156a09a1847a6c05b098a12484ddea
wangpiqi <737711464@qq.com>
2020-11-01 09:42:56 +0800

    1464. 数组中两元素的最大乘积

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
 class Solution {
 public:
+	//1464. 数组中两元素的最大乘积
+	int maxProduct(vector<int>& nums) {
+		nth_element(nums.begin(), nums.begin() + 2, nums.end(), std::greater<int>());
+		return (nums[0] - 1) * (nums[1] - 1);
+	}
+
 	//LCP 06. 拿硬币
 	int minCount(vector<int>& coins) {
 		int count{0};
